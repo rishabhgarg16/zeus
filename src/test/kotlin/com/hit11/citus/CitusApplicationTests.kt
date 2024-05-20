30e266098ae4cc2387ac0568c3680886babec374
Rishabh Garg <rishabhgarg@meta.com>
2024-05-20 11:24:16 +0100

    initial commit

---
+++ src/test/kotlin/com/hit11/citus/CitusApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hit11.citus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CitusApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
