064b6b958ff3402952230dbda08f6d715f9678ce
Anmol Singh <radbrawler@gmail.com>
2024-05-26 21:03:22 +0530

    reverting upcoming matches api

--- src/main/kotlin/com/hit11/zeus/controller/MatchController.kt
+++ src/main/kotlin/com/hit11/zeus/controller/MatchController.kt
@@ -1,24 +1,17 @@
 package com.hit11.zeus.controller
 
-import com.google.cloud.firestore.Firestore
-import com.google.firebase.cloud.FirestoreClient
 import com.hit11.zeus.model.Match
-import com.hit11.zeus.service.FirestoreService
 import com.hit11.zeus.service.MatchService
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/matches")
-class MatchController(private val matchService: MatchService, private val firestoreService: FirestoreService) {
+class MatchController(private val matchService: MatchService) {
 
-    var matchCollection = firestoreService.getCollection("test_matches")
-    private val firestore: Firestore = FirestoreClient.getFirestore()
-//    @GetMapping("/upcoming")
-//    fun getUpcomingMatches(): ResponseEntity<Any> {
-//
-//        return firestore.collection("test_matches").
-//    }
+    @GetMapping("/upcoming")
+    fun getUpcomingMatches(): List<Match> {
+        return matchService.getUpcomingMatches()
+    }
 }
