39f30e992a2470a70b320d854e38795aa381992e
Rishabh Garg <rishabhgarg@meta.com>
2024-05-30 00:16:20 +0100

    adding scripts

--- src/main/scripts/espn_scrape.py
+++ src/main/scripts/espn_scrape.py
@@ -2,104 +2,88 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# URL to scrape squads
-url = "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/squads"
-
-# Send a GET request to the URL
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-
-# Scraping squads and players details
-squads_section = soup.find_all('div', class_='ds-px-4 ds-py-3')
-
-squads_data = []
-
-for section in squads_section:
-    team_name_tag = section.find('h5')
-    if not team_name_tag:
-        continue
-
-    team_name = team_name_tag.text.strip()
-    players = section.find_all('a', class_='ds-inline-flex ds-items-start ds-leading-tight')
-
-    for player in players:
-        player_name = player.text.strip()
-        player_link = player['href']
-
-        squads_data.append({
-            'Team': team_name,
-            'Player Name': player_name,
-            'Player Profile Link': f"https://www.espncricinfo.com{player_link}"
-        })
-
-# Convert to DataFrame
-squads_df = pd.DataFrame(squads_data)
-
-# Save squads data to CSV
-squads_df.to_csv('squads.csv', index=False)
-
 # URL to scrape fixtures
 fixtures_url = "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/match-schedule-fixtures"
 response_fixtures = requests.get(fixtures_url)
 soup_fixtures = BeautifulSoup(response_fixtures.content, 'html.parser')
 
-fixtures_section = soup_fixtures.find_all('div', class_='ds-p-4 hover:ds-bg-ui-fill-translucent')
+# Possible div classes to look for fixture data
+div_classes = [
+    'ds-p-4 hover:ds-bg-ui-fill-translucent',
+    'ds-p-4 hover:ds-bg-ui-fill-translucent ds-border-none ds-border-t ds-border-line ds-bg-ui-fill-translucent-hover'
+]
 
 fixtures_data = []
+previous_date = 'N/A'
 
-for fixture in fixtures_section:
-    match_details_tag = fixture.find('a', class_='ds-no-tap-higlight')
-    if not match_details_tag:
-        continue
+for div_class in div_classes:
+    fixtures_section = soup_fixtures.find_all('div', class_=div_class)
 
-    match_link = match_details_tag['href']
-    match_summary = match_details_tag.find('div', class_='ds-text-compact-xxs').text.strip()
+    for fixture in fixtures_section:
+        # Extract match info
+        match_info_tag = fixture.find('div', class_='ds-text-tight-s ds-font-regular ds-truncate ds-text-typo-mid3')
+        if match_info_tag:
+            match_info_span = match_info_tag.find('span', class_='ds-text-tight-s ds-font-medium ds-text-typo')
+            if match_info_span:
+                match_info_parts = match_info_span.text.strip().split(',')
+                match_number = match_info_parts[0].strip() if len(match_info_parts) > 0 else 'N/A'
+                match_group = match_info_parts[1].strip() if len(match_info_parts) > 1 else 'N/A'
+                location = match_info_tag.text.split('•')[1].split(',')[0].strip() if '•' in match_info_tag.text else 'N/A'
+                tournament_type_tag = match_info_tag.find('span', class_='ds-inline-flex ds-items-start ds-leading-none !ds-inline')
+                tournament_type = tournament_type_tag.text.strip() if tournament_type_tag else 'N/A'
+            else:
+                match_number = match_group = location = tournament_type = 'N/A'
+        else:
+            match_number = match_group = location = tournament_type = 'N/A'
 
-    team_tags = fixture.find_all('p', class_='ds-text-tight-m ds-font-bold ds-capitalize ds-truncate')
-    if len(team_tags) == 2:
-        team_a = team_tags[0].text.strip()
-        team_b = team_tags[1].text.strip()
-    else:
-        team_a = team_b = 'N/A'
+        # Extract match summary
+        match_summary = match_info_tag.text.strip() if match_info_tag else 'N/A'
 
-    date_time_tag = fixture.find('div', class_='ds-text-tight-m ds-font-bold')
-    date_time = date_time_tag.text.strip() if date_time_tag else 'N/A'
+        date_tag = fixture.find('div', class_='ds-text-compact-xs ds-font-bold ds-w-24')
+        print(f"date tag: {date_tag}")
+        match_date = date_tag.text.strip() if date_tag else previous_date
+        print(f"match date: {match_date}")
 
-    time_details_tag = fixture.find('span', class_='ds-text-tight-xs')
-    time_details = time_details_tag.text.strip() if time_details_tag else 'N/A'
-    gmt_time = time_details.split('|')[0].strip() if '|' in time_details else 'N/A'
+        team_tags = fixture.find_all('p', class_='ds-text-tight-m ds-font-bold ds-capitalize ds-truncate')
+        if len(team_tags) == 2:
+            team_a = team_tags[0].text.strip()
+            team_b = team_tags[1].text.strip()
+        else:
+            team_a = team_b = 'N/A'
 
-    location_tag = fixture.find('div', class_='ds-truncate ds-text-tight-s ds-font-medium ds-text-typo-mid3')
-    location = location_tag.text.strip() if location_tag else 'N/A'
+        time_details_tag = fixture.find('span', class_='ds-text-tight-xs')
+        time_details = time_details_tag.text.strip() if time_details_tag else 'N/A'
+        gmt_time = time_details.split('|')[0].strip() if '|' in time_details else 'N/A'
 
-    location_parts = location.split(',')
-    if len(location_parts) > 1:
-        match_info = location_parts[0].split()
-        match_number = match_info[0] if match_info else 'N/A'
-        match_group = match_info[2] if len(match_info) >= 3 else 'N/A'
-    else:
-        match_number = match_group = 'N/A'
+        match_status_tag = fixture.find('p', class_='ds-text-tight-s ds-font-medium ds-line-clamp-2 ds-text-typo')
+        match_status = match_status_tag.text.strip() if match_status_tag else 'N/A'
 
-    match_status_tag = fixture.find('p', class_='ds-text-tight-s ds-font-medium ds-line-clamp-2 ds-text-typo')
-    match_status = match_status_tag.text.strip() if match_status_tag else 'N/A'
+        match_link_tag = fixture.find('a', class_='ds-no-tap-higlight')
+        match_link = f"https://www.espncricinfo.com{match_link_tag['href']}" if match_link_tag else 'N/A'
 
-    fixtures_data.append({
-        'Match': match_summary,
-        'Team A': team_a,
-        'Team B': team_b,
-        'Date': date_time.split('T')[0] if 'T' in date_time else date_time,  # Extracting date if present
-        'Time (GMT)': gmt_time,
-        'Match Number': match_number,
-        'Match Group': match_group,
-        'Location': location,
-        'Match Status': match_status,
-        'Match Link': f"https://www.espncricinfo.com{match_link}"
-    })
+        fixtures_data.append({
+            'Match Number': match_number,
+            'Match Group': match_group,
+            'Match Summary': match_summary,
+            'Team A': team_a,
+            'Team B': team_b,
+            'Date': match_date,
+            'Time (GMT)': gmt_time,
+            'Location': location,
+            'Tournament Type': tournament_type,
+            'Match Status': match_status,
+            'Match Link': match_link
+        })
+
+
+        # Update the previous_date if date_tag was present
+        if date_tag:
+            previous_date = match_date
 
 # Convert to DataFrame
 fixtures_df = pd.DataFrame(fixtures_data)
 
 # Save fixtures data to CSV
-fixtures_df.to_csv('fixtures5.csv', index=False)
+fixtures_df.to_csv('fixtures.csv', index=False)
 
-print("Scraping completed! Data saved to squads.csv and fixtures.csv.")
+print("Scraping completed! Data saved to fixtures.csv.")
--- src/main/scripts/espn_squad_squads.py
+++ src/main/scripts/espn_squad_squads.py
@@ -4,15 +4,26 @@ import pandas as pd
 
 # List of squad URLs
 squad_urls = [
-    # "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/afghanistan-squad-1431702/series-squads",
-    # "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/australia-squad-1431715/series-squads",
-    # "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/bangladesh-squad-1433740/series-squads",
-    # "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/canada-squad-1431914/series-squads",
-    # "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/england-squad-1431604/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/afghanistan-squad-1431702/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/australia-squad-1431715/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/bangladesh-squad-1433740/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/canada-squad-1431914/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/england-squad-1431604/series-squads",
     "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/india-squad-1431601/series-squads",
-    # "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/ireland-squad-1432769/series-squads",
-    # "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/namibia-squad-1433240/series-squads",
-    # "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/nepal-squad-1431788/series-squads"
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/ireland-squad-1432769/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/nepal-squad-1431788/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/netherlands-squad-1433671/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/new-zealand-squad-1431440/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/namibia-squad-1433240/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/oman-squad-1431785/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/pakistan-squad-1435201/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/papua-new-guinea-squad-1432846/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/scotland-squad-1432589/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/south-africa-squad-1431579/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/sri-lanka-squad-1433061/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/uganda-squad-1432588/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/united-states-of-america-squad-1432101/series-squads",
+    "https://www.espncricinfo.com/series/icc-men-s-t20-world-cup-2024-1411166/west-indies-squad-1432115/series-squads"
 ]
 
 squads_data = []
@@ -34,8 +45,7 @@ def scrape_squad(url):
         if not player_name_tag:
             continue
         player_name = player_name_tag.text.strip()
-
-        print(f"player name: {player_name_tag}")
+        # print(f"Player Name: {player_name}")
 
         # Extract player image
         player_image_tag = player_section.find('span', class_='ds-border ds-border-line-default-translucent ds-text-typo ds-bg-ui-fill ds-overflow-hidden ds-flex ds-items-center ds-justify-center ds-w-12 ds-h-12 ds-rounded-full').find('img')
@@ -46,15 +56,23 @@ def scrape_squad(url):
         player_role = player_role_tag.text.strip() if player_role_tag else 'N/A'
 
         # Extract player age
-        age_tag = player_section.find('div', class_='ds-flex ds-items-center ds-space-x-1').find_all('span', class_='ds-text-compact-xxs ds-font-bold')[0]
-        player_age = age_tag.text.strip() if age_tag else 'N/A'
+        age_tags = player_section.find_all('span', class_='ds-text-compact-xxs ds-font-bold')
+        player_age = age_tags[0].text.strip() if len(age_tags) > 0 else 'N/A'
+
+        # Initialize batting and bowling styles
+        player_batting_style = 'N/A'
+        player_bowling_style = 'N/A'
 
         # Extract batting and bowling styles
-        batting_tag = player_section.find_all('div', class_='ds-flex ds-items-start ds-space-x-1')[0].find_all('span', class_='ds-text-compact-xxs ds-font-bold')[1]
-        player_batting_style = batting_tag.text.strip() if batting_tag else 'N/A'
+        style_tags = player_section.find_all('div', class_='ds-flex ds-items-start ds-space-x-1')
 
-        bowling_tag = player_section.find_all('div', class_='ds-flex ds-items-start ds-space-x-1')[1].find_all('span', class_='ds-text-compact-xxs ds-font-bold')[1] if len(player_section.find_all('div', class_='ds-flex ds-items-start ds-space-x-1')) > 1 else None
-        player_bowling_style = bowling_tag.text.strip() if bowling_tag else 'N/A'
+        for tag in style_tags:
+            tag_text = tag.find_all('span', class_='ds-text-compact-xxs')
+            if len(tag_text) > 1:
+                if 'Batting' in tag_text[0].text:
+                    player_batting_style = tag_text[1].text.strip()
+                elif 'Bowling' in tag_text[0].text:
+                    player_bowling_style = tag_text[1].text.strip()
 
         squads_data.append({
             'Team': team_name,
@@ -65,6 +83,7 @@ def scrape_squad(url):
             'Batting Style': player_batting_style,
             'Bowling Style': player_bowling_style
         })
+
 
 # Iterate through each squad URL and scrape data
 for url in squad_urls:
