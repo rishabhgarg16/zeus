f5383d858036dad9f3d77b0afde340b1df4cb76e
Rishabh Garg <rishabhgarg@meta.com>
2025-02-20 08:40:46 +0000

    wallet transactions api

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,7 +66,7 @@ publishing {
             groupId = "ai.hit11"
             artifactId = "zeus"
             // if getting 409, bump the version
-            version = "0.8.15"
+            version = "0.8.16"
         }
     }
     repositories {
@@ -77,7 +77,7 @@ publishing {
                 // if getting 401, run
                 // aws codeartifact get-authorization-token --domain hitcentral --domain-owner 590183692348 --region ap-south-1 --query authorizationToken --output text
                 password =
-                    "eyJ2ZXIiOjEsImlzdSI6MTczOTk4NTgzMCwiZW5jIjoiQTEyOEdDTSIsInRhZyI6Im5VQVEzeHpCNG5lelpqdHBBMWVHQ3ciLCJleHAiOjE3NDAwMjkwMzAsImFsZyI6IkExMjhHQ01LVyIsIml2IjoienZocm9Nam1PLXkxN05BdSJ9.FbA1iQe7ivc2oaA8TviaVg.OBuib6hQ8uCQ2AXa.4Fd24wO7Lh9jDb70pqYVRUS-KQRDfFb8uqhQqZ-7pRfh_DZwgslMUyq9Rb8S-LZOMG7LXzoJR5ptuz9NEIjg9eOVSshQN3_aOjaX1qrI6oLxFq78YQPIqps56nu6phSlZRgZtuwHUE022Ijtna2Nd-YvHtk2kmHLQb6ptxs72W13L3Dm25fL8_YFj7gL8y_f8NSzC3b8cxzPaSuJU7QT2Kr0jJSZjbO1D1-1hIUfOP6ceWZ38fpWokxVTH1_ZABjQGOiGiniu5RF2Aj8iWL2X-76ag_MpKtQLZf6s-dp1L0Bz9l_TfHhQW_cxSh2WRPmHC4VWIFoIDKX7aIjiGc_qQCqXv3HCQ73HLtsBxgiMax9uUAEdeT4V56SiQsxn95gQJmhmDc7c-qKCumfia90Loa5oQ_7NvKVculKTm9YUi_7VI2w_K7spQHV96F1mIohKGPep7qJjjEHl9P2OZu1s5loemOUNgK6wEqgbyRkN4Ym4k-LoX_MuV56_Q8ddUKIemfO7gKUOC7Ql8OzHOMb1s3qLs6wlgBzOpBseMmNDcfQk1qv1-7f_B5Y-COnHcA8bdPZp0J-J33WBYrPntOMJFWfU3vNzCQffCV2tFRnblLEjpheIn1iIF5MLTvlTzrrmzLfx2gIS8kPCRWmTA2zvwm3TQSTli_PAlnbmsGH3EvgyQfdxbO1QMXWkvKJ8eupvo7FVZyRhi8P58iYb22YUb_ELLoPbVhMcJzo-0kON73FPN4xLzXsNdksCx5VP8sLyH8ObTC57INNwkY8NyqbpTGtL6DXvkEmBZyJ7hRzCRUJRO-PP3QCMRB1SY-FNQQUWgnMwogKkROmpOt10I9FLCYH7gaYMWbsHWqaRaLXK0GVXIKPtyy9LHPq1u7NdFyQaDCNl9bcfIr-297ZpMdxSSltpbXF2Hd-kbfAkY8v-9fQLfU1M48PN9zM7cG2gOTIBsz5U7-0-0RkbWpCUqXtjAPf8YDyPZIOs9XcoWKKlRwfew.0T1NjzApjx1_a_vWqpvDOg"
+                    "eyJ2ZXIiOjEsImlzdSI6MTc0MDA0MDUwMSwiZW5jIjoiQTEyOEdDTSIsInRhZyI6ImUxazVqcGJWb3ZCS3BzRGFPSXlmU3ciLCJleHAiOjE3NDAwODM3MDEsImFsZyI6IkExMjhHQ01LVyIsIml2IjoiV2JEeFpXSTJzbDNFNlN2RiJ9.AwoUeFlwvFWE2kO6YGHJlA.ZrijBJSDjn0ufYUO.Zk7RqKeBjGzB9zD6Po-ER720X_sL_pO5Iu1PR3cKfseLwtGWnxpTF358U9pgBpnxJhExNPIUTgI9P0zzdmq5LIX1-TQ7M_qahOBKIbnnnt0RblzBI3R6N8lf3HEIXHdx4acIqsVO_FR750HLGqBzY7Uqe11MRg_dmWbWs3tYPbn09kYwbLpKNcavtNqI_y8bXIPVtwnPMFWIfRcU3nTG2S4SFXnlpDj67LhunVywDXrsxAy5tfvWMSftoSyTCJr3IQOhPRuSOGvj6xMZmAZmz-PAvET6Vuk-nqzbarKvaa12QK_3mIuEBsedhmUwyJO3yw0ar7J_9ZITib-utrVfmEFfQmDOTnXpsekQQI-jI-MPH0MyBwfAl8_HF_82CZssTm1ucCV6YnAKg7RGbNg6LjdXjdPHWsC-8eHEpI2vBfds41Ef6tydRlLikdagCcogyXd0GnsaiLNMJUja_k5XuUGCnvGGm20qkAAq0zwLu6AsYLmxE-O5MTcp-iH1Vp-HOGQ3gkHqWW_1FgSTMBvrDMU6TjlQ95NEEI3OkCbe5nHM5yToP2FLQ4qoqnpqkqHywl9kd0QbdmZyHAULWCgqSsT9YTT524Ieia6SeDXVaueNPC8HIr8Sj5GTBy-DtZoNS-CWoh_VFBjGyGxyPZKTWxM4mgEEfibPtWBzcrwdkW6Zgmqv9c-uafA-F8J5xi2WyiFloWEL-SZnDVTzdxxQw_-remg-2voVKKhbOl1RYCyxTVbm0_pMB9RmZr8jvseMUHAQT3MrxGoCIVTS_pA8BF9Id9NGVSEnRhzDptjMc1B-80BlZ-VKUQEQ4yB8NtFFipJnVH1bkQKtrFlaen5DkbxMLesraFcRutfH7U5vflbdUoZxud_uzT9g9u1sLg5i8S0e_NsaQvKIu7Urj_dXFgogztCo4DxWtcbGs9iF1HWJZtgKBagsSGwT4HPRfL1371w5nCft9wagmcUY4OhHndi7zSCpQCp7oKX99QZVoxM.8ipC2boC78GdSYVn4_M3mQ"
             }
         }
     }
